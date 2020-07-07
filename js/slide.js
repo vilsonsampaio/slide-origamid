435c05717a5d0cb00737baf97900d595cf22226a
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-07-07 01:08:49 -0300

    Adicionando os touchevents (mobile)

--- js/slide.js
+++ js/slide.js
@@ -2,43 +2,54 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
-    this.dist = { finalPosition: 0, startX: 0, movement: 0 }
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 };
   }
 
   moveSlide(distX) {
     this.dist.movePosition = distX;
-    this.slide.style.transform = `translate3d(${distX}px, 0px, 0px)`
+    this.slide.style.transform = `translate3d(${distX}px, 0px, 0px)`;
   }
 
   updatePosition(clientX) {
-    this.dist.movement = 1.6*(this.dist.startX - event.clientX);
+    this.dist.movement = 1.6*(this.dist.startX - clientX);
     return this.dist.finalPosition + this.dist.movement;
   }
 
   onStart(event) {
-    event.preventDefault();
-    this.dist.startX = event.clientX;
-    this.wrapper.addEventListener('mousemove', this.onMove);
-    console.log(this.dist.startX)
+    let movetype;
+    if(event.type === 'mousedown') {
+      event.preventDefault();
+      this.dist.startX = event.clientX;
+      movetype = 'mousemove';
+    } else {
+      this.dist.startX = event.changedTouches[0].clientX;
+      movetype = 'touchmove';
+    }
+    console.log(movetype)
+    this.wrapper.addEventListener(movetype, this.onMove);
   }
 
   onMove(event) {
-    event.preventDefault();
-    const finalPosition = this.updatePosition(event.clientX);
+    const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX;
+    const finalPosition = this.updatePosition(pointerPosition);
 
-    this.moveSlide(finalPosition)
-    console.log(finalPosition);
+    this.moveSlide(finalPosition);
   }
 
   onEnd(event) {
+    const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
     this.wrapper.removeEventListener('mousemove', this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
-    console.log(this.dist)
   }
 
   addSlideEvents() {
-    this.wrapper.addEventListener('mousedown', this.onStart)
+    this.wrapper.addEventListener('mousedown', this.onStart);
     this.wrapper.addEventListener('mouseup', this.onEnd);
+    
+    // Adicionando evento de toque 
+    this.wrapper.addEventListener('touchstart', this.onStart);
+    this.wrapper.addEventListener('touchend', this.onEnd);
+
   }
 
   bindEvents() {
